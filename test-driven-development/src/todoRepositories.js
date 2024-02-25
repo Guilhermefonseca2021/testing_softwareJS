4d2ea8a9cc191e3dbb7a18c8cc09c7e0c7082231
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-02-24 21:54:07 -0300

    list repo

---
+++ test-driven-development/src/todoRepositories.js
@@ -0,0 +1,22 @@
+const loki = require('lokijs')
+
+module.exports = class TodoRepository {
+    constructor() {
+        const db = new loki('todo', { })
+        this.schedule = db.addCollection('schedule')
+    }
+
+    list() {
+        return this.schedule.find()
+    }
+
+    create() {
+        return this.schedule.insertOne(data)
+    }
+}
+
+const c = new TodoRepository()
+c.create({ name: 'xuxadasilva', age: 90 })
+c.create({ name: 'jonaszinho', age: 44})
+
+console.log('list', c.list())
--- test-driven-development/test/todo.test.js
+++ test-driven-development/test/todo.test.js
@@ -16,8 +16,15 @@ describe("todo", () => {
       const result = todo.isValid();
       expect(result).to.be.not.ok;
     });
-    it("should return invalid when create an object without text", () => {});
-    it('should have "id", "text", "when" ans "status" properties after creating object', () => {});
+    it("should return invalid when create an object without text", () => {
+      
+    });
+    it('should have "id", "text", "when" ans "status" properties after creating object', () => {
+      const data = { text: "", when: new Date("20-02-01") };
+      const todo = new Todo(data);
+      const result = todo.isValid();
+      expect(result).to.be.ok;
+    });
   });
 });
 
